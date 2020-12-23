2aa634b3fe78d227bd07482dfb080154e02cc93f
Francisco de Assis Boldt <fboldt@gmail.com>
2020-12-23 11:49:08 -0300

    versão brasileira parametrizada

--- greet.py
+++ greet.py
@@ -1,5 +1,9 @@
+import sys
 def main():
-    print("Olá!")
+    if len(sys.argv)>1 and sys.argv[1]=='pt-br':
+        print("Olá!")
+    else:
+        print("Hello!")
 
 if __name__ == "__main__":
     main()
